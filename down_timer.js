b67928930d4f6e8079201c51393953c2d510e475
Babul Mirdha <babumirdha@gmail.com>
2020-08-09 17:05:02 +0600

    improved in update selector.

--- down_timer.js
+++ down_timer.js
@@ -1,16 +1,19 @@
 class DownTimer {
+  hours = 0;
   minutes = 0;
   interval = null;
-  END_SEC = 4;
-  seconds = this.END_SEC;
+  SECS_IN_A_MIN = 10;
+  MINS_IN_AN_HOURS = 10;
+  seconds = this.SECS_IN_A_MIN - 1;
   callback = null;
 
   constructor(minutes, selector) {
-    this.minutes = minutes;
+    this.hours = Math.floor(minutes / this.MINS_IN_AN_HOURS);
+    this.minutes = minutes % this.MINS_IN_AN_HOURS;
     this.selector = selector;
   }
 
-  start(callback) {
+  start(callback = null) {
     this.minutes--;
     this.callback = callback;
     this.interval = setInterval(this.countDown.bind(this), 1000);
@@ -21,18 +24,22 @@ class DownTimer {
     this.callback();
   }
 
-  countDown() {
-    $(this.selector).html(this.minutes + ":" + this.seconds);
+  updateSelector() {
+    $(this.selector).html(this.hours + ":" + this.minutes + ":" + this.seconds);
+  }
 
+  countDown() {
+    this.updateSelector();
     if (this.seconds > 0) {
       this.seconds--;
     } else {
-      this.seconds = this.END_SEC;
       this.minutes--;
-
       if (this.minutes <= 0) {
+        this.updateSelector();
         this.stop();
       }
+
+      this.seconds = this.SECS_IN_A_MIN - 1;
     }
   }
 }
